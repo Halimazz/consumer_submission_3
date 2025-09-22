445f892282890db34cecba1768e2069f6a148e17
halimalaziz <sanankciez@gmail.com>
2025-09-22 09:30:04 +0700

    Update consumer.js

--- src/consumer.js
+++ src/consumer.js
@@ -14,7 +14,7 @@ const init = async () => {
     const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
     const channel = await connection.createChannel();
 
-    const queue = "exports:songs";
+    const queue = "export:playlists";
     await channel.assertQueue(queue, {
       durable: true,
     });
